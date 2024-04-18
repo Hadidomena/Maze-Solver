7bd220859e95266175292fbe0870a47e2edfb954
Hadidomena <106683153+Hadidomena@users.noreply.github.com>
2024-04-18 02:09:46 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -1,31 +1,59 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "bfs.h"
+#include "directions.h"
 #include "movement.h"
 #include "queue.h"
-#include "bfs.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int main( int argc, char* argv[] ) {
+int main(int argc, char *argv[])
+{
 
     int len_of_line;
-    char* file_name = argv[1];
+    char *file_name = argv[1];
 
-    position * start = find_start(file_name);
-    position * end = find_end(file_name);
+    if (argc != 2)
+    {
+        printf("Invalid arguments, pass only one argument to function, filename of maze\n");
+        return 1;
+    }
 
-    // Traverse the file using BFS
-    len_of_line = bfs_traverse_file(file_name);
+    if (is_txt_file(file_name) == 0)
+    {
 
-    printf("After traverse\n");
-    remove("queue1.txt"); remove("queue2.txt");
-    backtrack(file_name, start->current_character, end->current_character, len_of_line);
+        position *start = find_start(file_name);
+        position *end = find_end(file_name);
 
-    printf("output created\n");
+        len_of_line = bfs_traverse_file(file_name);
+        remove("queue1.txt");
+        remove("queue2.txt");
 
-    reverseLines();
+        backtrack(file_name, start->current_character, end->current_character, len_of_line);
 
-    remove("output.txt");
+        reverseLines();
+        remove("output.txt");
+    }
+    else if (is_txt_file(file_name) == 1)
+    {
 
-    printf("End\n");
+        bin_to_txt(file_name, "temp_maze.txt");
+
+        position *start = find_start("temp_maze.txt");
+        position *end = find_end("temp_maze.txt");
+
+        len_of_line = bfs_traverse_file("temp_maze.txt");
+        remove("queue1.txt");
+        remove("queue2.txt");
+
+        backtrack("temp_maze.txt", start->current_character, end->current_character, len_of_line);
+
+        reverseLines();
+        remove("output.txt");
+        remove("temp_maze.txt");
+    }
+    else
+    {
+        printf("Not a proper filename.\n");
+    }
 
     return 0;
 }
--- queue.c
+++ queue.c
@@ -1,84 +1,32 @@
-#include "movement.h"
 #include "queue.h"
+#include "movement.h"
 #include <stdio.h>
 #include <stdlib.h>
-// Function to create a new node
-Node* newNode(position value) {
-    Node* temp = (Node*)malloc(sizeof(Node));
-    temp->value = value;
-    temp->next = NULL;
-    return temp;
-}
 
-// Function to create an empty queue
-Queue* createQueue() {
-    Queue* q = (Queue*)malloc(sizeof(Queue));
-    q->front = q->rear = NULL;
-    return q;
-}
-
-// Function to add an element to the queue
-void enqueue(Queue* q, position value) {
-    Node* temp = newNode(value);
-    if (q->rear == NULL) {
-        q->front = q->rear = temp;
-        return;
-    }
-    q->rear->next = temp;
-    q->rear = temp;
-}
-
-// Function to remove an element from the queue
-position dequeue(Queue* q) {
-    if (q->front == NULL) {
-        exit(EXIT_FAILURE);
-    }
-    Node* temp = q->front;
-    position value = temp->value;
-    q->front = q->front->next;
-
-    if (q->front == NULL)
-        q->rear = NULL;
-
-    free(temp);
-    return value;
-}
-
-// Function to check if the queue is empty
-int isEmpty(Queue* q) {
-    return (q->front == NULL);
-}
-void destroyQueue(Queue* q) {
-    if (q != NULL) {
-        Node* current = q->front;
-        while (current != NULL) {
-            Node* temp = current;
-            current = current->next;
-            free(temp); // Free memory allocated for node
-        }
-        free(q); // Free memory allocated for queue
-    }
-}
-
-void writeToQueue(const char *data, const char *filename) {
-    FILE *file = fopen(filename, "a"); // Open file in append mode
-    if (file == NULL) {
+// Function to write data to the file
+void writeToQueue(const char *data, const char *filename)
+{
+    FILE *file = fopen(filename, "a");
+    if (file == NULL)
+    {
         printf("Error opening file for writing.\n");
         return;
     }
-    fprintf(file, "%s\n", data); // Write data to the file
-    fclose(file); // Close the file
+    fprintf(file, "%s\n", data);
+    fclose(file);
 }
 
 // Function to read data from the file
-void readFromQueue(const char *filename, long *pos, char *buffer, size_t bufferSize) {
-    FILE *file = fopen(filename, "r"); // Open file in read mode
-    if (file == NULL) {
+void readFromQueue(const char *filename, long *pos, char *buffer, size_t bufferSize)
+{
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {
         printf("Error opening file for reading.\n");
         return;
     }
-    fseek(file, *pos, SEEK_SET); // Move the file pointer to the current position
-    fgets(buffer, bufferSize, file); // Read one line from the file into the buffer
-    *pos = ftell(file); // Update the current position
-    fclose(file); // Close the file
+    fseek(file, *pos, SEEK_SET);
+    fgets(buffer, bufferSize, file);
+    *pos = ftell(file);
+    fclose(file);
 }
